c8fa423e5d36f94177c56c061d293488c9b061e2
Aleksandr Matkovskiy <matkovskiy.aleksandr>
2021-02-14 18:28:12 +0200

    1

--- code/LCD_temp.py
+++ code/LCD_temp.py
@@ -25,7 +25,7 @@ try:
 
             print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
         else:
-            print("Failed to retrieve data from humidity sensor"
+            print("Failed to retrieve data from humidity sensor")
         
         
         display.lcd_clear()                               # Clear the display of any data       
